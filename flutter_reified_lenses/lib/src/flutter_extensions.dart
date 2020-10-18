b29cdbccd45a32865e8251d5ae2013961996d219
Garo Brik <garo.brik@gmail.com>
2020-10-18 00:44:35 -0400

    add key parameter to cursor's build method

--- flutter_reified_lenses/lib/src/flutter_extensions.dart
+++ flutter_reified_lenses/lib/src/flutter_extensions.dart
@@ -2,8 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:reified_lenses/reified_lenses.dart';
 
 extension GetCursorFlutterExtension<C extends GetCursor, S> on Zoom<C, S> {
-  Widget build(Widget Function(S) builder) {
-    return Builder(builder: (context) {
+  Widget build(Widget Function(S) builder, {Key key}) {
+    return Builder(key: key, builder: (context) {
       final value = this.get(() => (context as Element).markNeedsBuild());
       return builder(value);
     });
