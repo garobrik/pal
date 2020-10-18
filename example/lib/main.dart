4584a0f83f5a4610e07bfd52fb6de7c99ebdacc4
Garo Brik <garo.brik@gmail.com>
2020-10-18 00:46:47 -0400

    load of bug fixes re nested scroll view in example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,17 +16,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: Provider(
-        child: Center(
-            child: Consumer(
-          builder: (_, v, __) => Center(child: TableWidget(v.table)),
-        )),
+        child: Scaffold(
+          body: Consumer<ListenableState<Model.Table>>(
+            builder: (_, v, __) => Center(child: TableWidget(v.cursor)),
+          ),
+        ),
         create: (_) => ListenableState(Model.Table.from(columns: [
-          StringColumn.from(values: ['test', 'row'])
+              StringColumn.from(values: List.generate(50, (i) => "Row $i"))
         ])),
       ),
     );
@@ -39,46 +36,60 @@ class TableWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      buildHeader(),
-      table.length.build((length) => ReorderableSliverList(
-            onReorder: (a, b) {
-              table.columns.forEach((column) {
-                column.values[a].set(column.values[b].get(() {}));
-                column.values[b].set(column.values[a].get(() {}));
-              });
-            },
-            delegate: ReorderableSliverChildBuilderDelegate(
-              (_, i) => buildRow(i),
-              childCount: length,
-            ),
-          )),
-    ]);
+    ScrollController _scrollController =
+        PrimaryScrollController.of(context) ?? ScrollController();
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 500),
+        child: CustomScrollView(
+          controller: _scrollController,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          slivers: [
+            SliverList(delegate: SliverChildListDelegate([buildHeader()])),
+            table.length.build((length) => ReorderableSliverList(
+                  onReorder: (a, b) {
+                    table.columns.forEach((column) {
+                      column.values[a].set(column.values[b].get(() {}));
+                      column.values[b].set(column.values[a].get(() {}));
+                    });
+                  },
+                  delegate: ReorderableSliverChildBuilderDelegate(
+                    (_, i) => buildRow(i),
+                    childCount: length,
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget buildHeader() {
-    return table.columns.length.build((length) => ReorderableSliverList(
-          onReorder: (a, b) {
-            table.columns[a].set(table.columns[b].get(() {}));
-            table.columns[b].set(table.columns[a].get(() {}));
-          },
-          delegate: ReorderableSliverChildBuilderDelegate(
-            (_, i) => Text('header'),
-            childCount: length,
-          ),
-        ));
+    return table.columns.length.build((length) => Row(
+      // onReorder: (a, b) {
+      //   table.columns[a].set(table.columns[b].get(() {}));
+      //   table.columns[b].set(table.columns[a].get(() {}));
+      // },
+      children: List.generate(
+        length,
+        (columnIndex) => Text('header', key: ValueKey(columnIndex)),
+      ),
+    ));
   }
 
-  Widget buildRow(int index) {
-    return table.columns.length.build((length) => ReorderableSliverList(
-        onReorder: (a, b) {
-          table.columns[a].set(table.columns[b].get(() {}));
-          table.columns[b].set(table.columns[a].get(() {}));
-        },
-        delegate: ReorderableSliverChildBuilderDelegate(
-          (_, i) => Text(table.columns[i].values[index].get((){})),
-          childCount: length,
+  Widget buildRow(int rowIndex) {
+    return table.columns.length.build(
+      (length) => Row(
+        children: List.generate(
+          length,
+          (columnIndex) => table.columns[columnIndex].values[rowIndex].build(
+            (s) => Text(s),
+          ),
         ),
-    ));
+      ),
+      key: ValueKey(rowIndex),
+    );
   }
 }
