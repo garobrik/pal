a6d3f4d5d61c09dbb742e0225dfcccc23491cf51
Garo Brik <garo.brik@gmail.com>
2021-02-20 17:22:31 -0800

    add hash and equality helpers

--- reified_lenses/lib/reified_lenses.dart
+++ reified_lenses/lib/reified_lenses.dart
@@ -5,4 +5,5 @@ export 'src/reified_lenses/state_management.dart';
 export 'src/reified_lenses/trie_map.dart';
 export 'src/functional/vec.dart';
 export 'src/functional/tuple.dart';
+export 'src/functional/equality.dart';
 export 'annotations.dart';
---
+++ reified_lenses/lib/src/functional/equality.dart
@@ -0,0 +1,12 @@
+import 'package:reified_lenses/reified_lenses.dart';
+
+extension IterableEquality<V> on Iterable<V> {
+  bool iterableEqual(Iterable<V> other) =>
+      zip(this, other).any((pair) => pair.first == pair.second);
+}
+
+int hash(Iterable iterable) {
+  int result = 1;
+  for (final value in iterable) result = 31 * result + value.hashCode;
+  return result;
+}
