526988c58b5fd85b3ff4934095afa832e875ffb5
shrouxm <mushrouxm@gmail.com>
2021-08-14 10:09:49 -0700

    dropdown close on esc

--- knose/lib/src/infra_widgets/dropdown.dart
+++ knose/lib/src/infra_widgets/dropdown.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_reified_lenses/flutter_reified_lenses.dart';
 
@@ -193,6 +194,13 @@ Widget _oldDropdown({
         numChildren: numChildren,
         child: FocusTraversalGroup(
           child: Focus(
+            onKey: (node, key) {
+              if (key.logicalKey == LogicalKeyboardKey.escape) {
+                isOpen.set(false);
+                return KeyEventResult.handled;
+              }
+              return KeyEventResult.ignored;
+            },
             skipTraversal: true,
             onFocusChange: isOpen.set,
             child: Material(
