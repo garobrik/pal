991978c7bc9110570752eebaa2a4ae37362bd85a
shrouxm <mushrouxm@gmail.com>
2022-04-25 10:31:35 -0700

    make pairs hashable/equatable

--- reified_lenses/lib/src/functional/tuple.dart
+++ reified_lenses/lib/src/functional/tuple.dart
@@ -12,6 +12,14 @@ class Pair<A, B> with _PairMixin<A, B> {
   final B second;
 
   Pair(this.first, this.second);
+
+  @override
+  int get hashCode => hash(<dynamic>[first, second]);
+
+  @override
+  bool operator ==(Object other) {
+    return other is Pair<A, B> && first == other.first && second == other.second;
+  }
 }
 
 Iterable<Pair<A, B>> zip<A, B>(
