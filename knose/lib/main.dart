623c14b172e06d7044a7e792149db0f550eae3c4
shrouxm <mushrouxm@gmail.com>
2022-10-17 16:38:02 -0700

    commit temporary testthingy main for convenience

--- knose/lib/main.dart
+++ knose/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:ctx/ctx.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reified_lenses/flutter_reified_lenses.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:knose/pal.dart';
+import 'package:knose/pal.dart' hide number, Literal, Text, text, Type, TypeTree, ID, Expr;
+import 'package:knose/src/app_widgets/pal_editor.dart';
 import 'package:knose/theme.dart';
 import 'package:knose/model.dart' as model;
 import 'package:knose/app_widgets.dart';
@@ -28,6 +29,65 @@ Widget myApp() {
           actions: actions,
           theme: theme(Colors.grey, Brightness.light),
           onGenerateRoute: (settings) {
+            if (settings.name == '/') {
+              return MaterialPageRoute<void>(
+                builder: (context) {
+                  return const Scaffold(
+                    body: DeferredPaintTarget(
+                      child: TestThingy(Ctx.empty),
+                    ),
+                  );
+                  // return ReaderWidget(
+                  //   ctx: Ctx.empty,
+                  //   builder: (context, ctx) {
+                  //     final bindingID = ID('binding');
+                  //     final expr = useCursor(placeholder);
+                  //     final binding = useMemoized(
+                  //       () => Binding.mk(
+                  //         id: bindingID,
+                  //         type: Fn.type(argType: number, returnType: number),
+                  //         name: 'the function!',
+                  //         value: Optional(Expr.data(Fn.from(
+                  //           argName: 'numba',
+                  //           type: Fn.type(argType: number, returnType: number),
+                  //           body: (arg) => arg,
+                  //         ))),
+                  //       ),
+                  //     );
+                  //     final testCtx = coreCtx.withBinding(binding);
+                  //     return Scaffold(
+                  //       body: DeferredPaintTarget(
+                  //         child: Column(
+                  //           crossAxisAlignment: CrossAxisAlignment.start,
+                  //           children: [
+                  //             ExprEditor(testCtx, expr),
+                  //             const Divider(),
+                  //             ReaderWidget(
+                  //               ctx: ctx,
+                  //               builder: (_, ctx) {
+                  //                 final type = typeCheck(testCtx, expr.read(ctx));
+                  //                 return Option.cases(
+                  //                   type,
+                  //                   some: (type) {
+                  //                     final typeDef = testCtx.getType(Type.id(type));
+                  //                     return Text(
+                  //                       '${TypeTree.name(TypeDef.tree(typeDef))}: ${eval(testCtx, expr.read(ctx))}',
+                  //                     );
+                  //                   },
+                  //                   none: () => const Text('errorr!'),
+                  //                 );
+                  //               },
+                  //             ),
+                  //           ],
+                  //         ),
+                  //       ),
+                  //     );
+                  //   },
+                  // );
+                },
+              );
+            }
+
             if (settings.name == '/') {
               return generateSearchRoute(ctx.withDB(db));
             }
